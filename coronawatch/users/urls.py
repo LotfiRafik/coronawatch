7abf3a6f43825834cc5d07f9da60b142fc7205bb
root <root@localhost.localdomain>
2020-03-15 10:13:42 +0100

    Travis conf file for Continuous Integration

--- coronawatch/users/urls.py
+++ coronawatch/users/urls.py
@@ -3,10 +3,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from users.views import UserDetail, UsersList, GoogleSign, EmailSign, AdminSign
 
 urlpatterns = [
-    path('/', UsersList.as_view()),
-    path('/<int:pk>/', UserDetail.as_view()),
-    path('/googlesign/', GoogleSign.as_view()),
-    path('/emailsign/', EmailSign.as_view()),
-    path('/adminsign/', AdminSign.as_view()),
-    path('/login/', obtain_auth_token)
+    path('', UsersList.as_view()),
+    path('<int:pk>/', UserDetail.as_view()),
+    path('googlesign/', GoogleSign.as_view()),
+    path('emailsign/', EmailSign.as_view()),
+    path('adminsign/', AdminSign.as_view()),
+    path('login/', obtain_auth_token)
 ]
