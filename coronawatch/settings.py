97aa67db2d50859f81a326c87f3b3731644e25d5
root <root@localhost.localdomain>
2020-03-23 23:51:49 +0100

    conf postgres for Travis CI

--- coronawatch/settings.py
+++ coronawatch/settings.py
@@ -104,13 +104,14 @@ WSGI_APPLICATION = 'coronawatch.wsgi.application'
 
 
 # Heroku: Update database configuration from $DATABASE_URL. 
-ON_HEROKU = os.environ.get('ON_HEROKU')
-if not ON_HEROKU:
-    DATABASE_URL="postgres://postgres:postgres@localhost:5432/coronawatch"
+# ON_HEROKU = os.environ.get('ON_HEROKU')
+# if not ON_HEROKU:
+#     DATABASE_URL="postgres://postgres:postgres@localhost:5432/coronawatch"
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
-#DATABASES = {}
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
