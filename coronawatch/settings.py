c6f1d41091c7fe59abdb6967019245b4896b640c
LotfiRafik <42896427+LotfiRafik@users.noreply.github.com>
2020-03-23 17:00:17 +0100

    Update settings.py

--- coronawatch/settings.py
+++ coronawatch/settings.py
@@ -98,10 +98,13 @@ WSGI_APPLICATION = 'coronawatch.wsgi.application'
 
 
 # Heroku: Update database configuration from $DATABASE_URL. 
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if not ON_HEROKU:
+    DATABASE_URL=postgres://postgres:postgres@localhost:5432/coronawatch
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+#DATABASES = {}
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
