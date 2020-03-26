e1e8df70906aeddc9b30086ca0b24ffdde3a0fbd
root <root@localhost.localdomain>
2020-03-27 00:30:58 +0100

    testing heroku upload to cloudniray

--- articles/views.py
+++ articles/views.py
@@ -55,10 +55,16 @@ class NewArticle(APIView):
     for f in request.FILES.getlist('attachments'):
       #if file is TemporaryUploadFile type , we pass the path 
       if isinstance(f, TemporaryUploadedFile):
+        print ("is instance of Temprary Uploaded File")
+        sys.stdout.flush()
         f = f.temporary_file_path()
         #Create temp folder to store the file before script end and the file get deleted
         if not os.path.exists('tmp/'):
+          print ("tmp/ doesnt exist")
+          sys.stdout.flush()
           os.makedirs('tmp/')
+          if os.path.exists('tmp/'):
+            print("tmp/ created")
         shutil.copyfile(f, f[1:])
         f = f[1:]
       django_rq.enqueue(upload_file_cloudinary, f, article)
@@ -69,23 +75,23 @@ class NewArticle(APIView):
 #upload image or video to cloud
 #update database (attachement_table)
 def upload_file_cloudinary(f,article):
-  at_type = ""
-  try:
-    extension = str(f).split(".")[1].lower()
-    if str(f).lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-      at_type = "photo"
-      out = cloudinary.uploader.upload(f, folder="articles")
-      attachmentArticle.objects.create(attachment_type=at_type, path=out['url'],articleid=article)
-    elif str(f).lower().endswith(('.mp4')):
-      at_type = "video"
-      out = cloudinary.uploader.upload(f, resource_type = "video", folder="articles")
-      attachmentArticle.objects.create(attachment_type=at_type, path=out['url'],articleid=article)
-      #Remove file from tmp folder 
-      if os.path.exists(f):
-        os.remove(f)
-  except cloudinary.exceptions.Error:
-    print(cloudinary.exceptions.Error)
-    return Response(cloudinary.exceptions.Error, status=status.HTTP_400_BAD_REQUEST)
+  # at_type = ""
+  # try:
+  #   extension = str(f).split(".")[1].lower()
+  #   if str(f).lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+  #     at_type = "photo"
+  #     out = cloudinary.uploader.upload(f, folder="articles")
+  #     attachmentArticle.objects.create(attachment_type=at_type, path=out['url'],articleid=article)
+  #   elif str(f).lower().endswith(('.mp4')):
+  #     at_type = "video"
+  #     out = cloudinary.uploader.upload(f, resource_type = "video", folder="articles")
+  #     attachmentArticle.objects.create(attachment_type=at_type, path=out['url'],articleid=article)
+  #     #Remove file from tmp folder 
+  #     if os.path.exists(f):
+  #       os.remove(f)
+  # except cloudinary.exceptions.Error:
+  #   print(cloudinary.exceptions.Error)
+  #   return Response(cloudinary.exceptions.Error, status=status.HTTP_400_BAD_REQUEST)
 
   return 0
 
