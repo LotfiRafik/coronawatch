96f5ed0caf9cdbb5e7ca5c29845dbd29c320bf8e
LotfiRafik <42896427+LotfiRafik@users.noreply.github.com>
2020-03-26 15:49:15 +0100

    Update settings.py

--- coronawatch/settings.py
+++ coronawatch/settings.py
@@ -205,14 +205,14 @@ cloudinary.config(
 RQ_QUEUES = {
     'default': {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
-        'DEFAULT_TIMEOUT': 500,
+        'DEFAULT_TIMEOUT': 5000,
     },
     'high': {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
-        'DEFAULT_TIMEOUT': 500,
+        'DEFAULT_TIMEOUT': 5000,
     },
     'low': {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
-        'DEFAULT_TIMEOUT': 500,
+        'DEFAULT_TIMEOUT': 5000,
     }
 }
