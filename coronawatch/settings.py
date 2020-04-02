8e8ab78ba28b11777cb615565414873aebaa1334
root <root@localhost.localdomain>
2020-04-02 20:52:14 +0100

    fix bug CORS_ORIGIN_ALLOW_ALL

--- coronawatch/settings.py
+++ coronawatch/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'cloudinary',
@@ -71,7 +72,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -191,3 +193,5 @@ cloudinary.config(
   api_secret = "KgP6a1XyiRDVaKNdGZsPQicbzm4" 
 )
 
+
+CORS_ORIGIN_ALLOW_ALL = True
