6de34c308aa3f1e084898dcd09f73fe16c6fba26
root <root@localhost.localdomain>
2020-04-02 21:58:47 +0100

    feat return user information with token in login service

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from users.views import UserDetail, UsersList, GoogleSign, EmailSign, AdminSign, AdminDetail,ModeratorDetail,AgentDetail,RedactorDetail,MobileDetail
+#from rest_framework.authtoken.views import obtain_auth_token
+from users.views import ObtainAuthToken, UserDetail, UsersList, GoogleSign, EmailSign, AdminSign, AdminDetail,ModeratorDetail,AgentDetail,RedactorDetail,MobileDetail
 
 urlpatterns = [
     path('', UsersList.as_view()),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('googlesign/', GoogleSign.as_view()),
     path('emailsign/', EmailSign.as_view()),
     path('adminsign/', AdminSign.as_view()),
-    path('login/', obtain_auth_token)
+    path('login/', ObtainAuthToken.as_view())
 ]
