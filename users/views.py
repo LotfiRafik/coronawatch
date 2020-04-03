831e8c07bd8e398edd2e3b07d46c94ec90d9f9c5
Bouchafa_Lotfi_Rafik <42896427+LotfiRafik@users.noreply.github.com>
2020-04-03 18:18:33 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -84,9 +84,11 @@ class EmailSign(APIView):
 
 #permission to admin only
 class AdminSign(APIView):
-    permission_classes = [IsAuthenticated, AdminOnly]
+    #permission_classes = [IsAuthenticated, AdminOnly]
 
     def post(self, request):
+        print(request.headers)
+        sys.stdout.flush()
         response = {}
         serializer = EmailSignSerializer(data=request.data)
         if serializer.is_valid():
