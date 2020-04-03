b013590d3966f816a8bb0ccda421db4d30dfb7cb
Bouchafa_Lotfi_Rafik <42896427+LotfiRafik@users.noreply.github.com>
2020-04-03 17:32:57 +0100

    Update settings.py

--- coronawatch/settings.py
+++ coronawatch/settings.py
@@ -195,3 +195,16 @@ cloudinary.config(
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'Authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
